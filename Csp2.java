41e3c78bb0eb2b4742f1530da70abb784320b2ee
vtghsr <1027103594@qq.com>
2020-12-11 10:53:44 +0800

    消除类游戏

--- Csp2.java
+++ Csp2.java
@@ -1,61 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.concurrent.PriorityBlockingQueue;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-public class Csp2{
+
+public class Csp2 {
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int m=sc.nextInt();
-		int[][] mat=new int[n][m];
+		int[][] board=new int[n][m];
+		int[][] bak=new int[n][m];
 		for (int i=0;i<n;i++) {
 			for (int j=0;j<m;j++) {
-				mat[i][j]=sc.nextInt();
+				int x=sc.nextInt();
+				board[i][j]=x;
+				bak[i][j]=x;
 			}
 		}
-		for (int[] row:mat) {
-			int i=0,j;//i是慢指针，j是快指针
+		//消除横向连续
+		for (int[] row:board) {
+			int i=0,j=0;
 			while (i<m) {
 				j=i;
 				while (j<m && row[i]==row[j]) {
 					j++;
 				}
-				if (j-i>=3){
+				if (j-i>=3) {
 					for (int k=i;k<j;k++) {
 						row[k]=0;
 					}
 				}
 				i=j;
-			}		
+			}
 		}
+		//消除纵向连续
 		for (int c=0;c<m;c++) {
-			int i=0,j;
+			int i=0,j=0;
 			while (i<n) {
 				j=i;
-				while (j<n && (mat[i][c]==mat[j][c] || mat[i][c]==0 || mat[j][c]==0)) {
-					j++; 
+				while (j<n && bak[i][c]==bak[j][c]) {
+					j++;
 				}
 				if (j-i>=3) {
 					for (int k=i;k<j;k++) {
-						mat[k][c]=0;
+						board[k][c]=0;
 					}
 				}
 				i=j;
@@ -63,10 +50,13 @@ public class Csp2{
 		}
 		for (int i=0;i<n;i++) {
 			for (int j=0;j<m-1;j++) {
-				System.out.format("%d ", mat[i][j]);
+				System.out.format("%d ", board[i][j]);
 			}
-			System.out.format("%d\n", mat[i][m-1]);
+			System.out.format("%d\n", board[i][m-1]);
 		}
+			
+		
 		
 	}
+
 }
